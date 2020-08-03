6fb03de652692db82e20a3a560a88320301216ad
Hannah Livnat <hannahlivnat@gmail.com>
2020-08-03 13:38:00 -0700

    Revert "additional chat options"
    This reverts commit f57898f2ebf0c4f41b583240ae37c6dac79a2f2b.

--- features/sample_hears.js
+++ features/sample_hears.js
@@ -38,35 +38,4 @@ module.exports = function(controller) {
     controller.hears(async (message) => message.text && message.text.toLowerCase() === 'certify', ['message'], async (bot, message) => {
         await bot.reply(message, 'After your account is set up, you must certify for your benefit payments every two weeks. This tells us if you are still unemployed and eligible to receive payments.');
     });
-
-    controller.hears(new RegExp(/\b(?:certify|certify for benefits|get benefits|certification)\b/gi), 'message', async (bot, message) => {
-
-        await bot.reply(message, {
-            text: 'After your account is set up, you must certify for your benefit payments every two weeks. This tells us if you are still unemployed and eligible to receive payments.'
-        });
-    });
-
-    controller.hears(new RegExp(/\b(?:payment|where's my payment|where payment|pay|money|missing benefits|didn't get benefits|no benefits|did not get benefits)\b/gi), 'message', async (bot, message) => {
-
-        await bot.reply(message, {
-            text: 'It will take about 10 days to process your certification if you submitted it by mail and/or requested your benefits by check. If it has been more than 10 days ...'
-        });
-    });
-
-    controller.hears(new RegExp(/\b(?:site not working|tech issues|tech problems|technical issue|help with site|technical support)\b/gi), 'message', async (bot, message) => {
-
-        await bot.reply(message, {
-            text: 'To get technical help with registration, password resets, EDD Account Numbers, and how to use UI Online : Call 1-833-978-2511 (English and Spanish) from 8 AM to 8 PM pst.'
-        });
-    });
-
-    controller.hears(new RegExp(/\b(?:covid-19|covid)\b/gi), 'message', async (bot, message) => {
-
-        await bot.reply(message, {
-            text: 'The Pandemic Unemployment Assistance is helping unemployed Californians who are not usually eligible for regular unemployment insurance. This includes business owners, self-employed workers, independing contractors, and those with limited work experience. The program includes up to 46 weeks of benefits from February 2 2020 through December26 2020. It launched with up to 39 weeks of benefits, and an extra 7 weeks was recently added.'
-        });
-    });
-
-
 }
-
