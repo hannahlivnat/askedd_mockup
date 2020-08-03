e7c999c47a697fe7f0b2d449a3844034f4bcec60
Hannah Livnat <hannahlivnat@gmail.com>
2020-08-03 14:17:18 -0700

    fixed prev reverts

--- features/sample_hears.js
+++ features/sample_hears.js
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-module.exports = function(controller) {
+module.exports = function (controller) {
 
     //// use a function to match a condition in the message
     //controller.hears(async (message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
@@ -19,12 +19,46 @@ module.exports = function(controller) {
     //    await bot.reply(message,{ text: 'I HEARD ALL CAPS!' });
     //});
 
-    controller.hears(['greeting', new RegExp(/\b(?:hello|hi|howdy|greetings|hola|oi|hey)\b/gi)], 'message', async(bot, message) => {
-        await bot.reply(message, 'Hello! How can we help you? You can type "quick" to view your options');
-    })
+    //controller.hears(['greeting', new RegExp(/\b(?:hello|hi|howdy|greetings|hola|oi|hey)\b/gi)], 'message', async(bot, message) => {
+    //    await bot.reply(message, 'Hello! How can we help you? You can type "quick" to view your options');
+    //})
 
-    controller.hears(['payments'], new RegExp(/\b(?:payments|pay|payment|money|paymnt)\b/gi), 'message', async(bot, message) => {
-        await bot.reply(message, '')
-    })
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'payments', ['message'], async (bot, message) => {
+        await bot.reply(message, 'It will take about 10 days to process your certification if you submitted it by mail and/or requested your benefits by check. If it has been more than 10 days ...');
+    });
+
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'tech issues', ['message'], async (bot, message) => {
+        await bot.reply(message, 'To get technical help with registration, password resets, EDD Account Numbers, and how to use UI Online : Call 1-833-978-2511 (English and Spanish) from 8 AM to 8 PM pst.');
+    });
+
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'covid-19', ['message'], async (bot, message) => {
+        await bot.reply(message, 'The Pandemic Unemployment Assistance is helping unemployed Californians who are not usually eligible for regular unemployment insurance. This includes business owners, self-employed workers, independing contractors, and those with limited work experience. The program includes up to 46 weeks of benefits from February 2 2020 through December26 2020. It launched with up to 39 weeks of benefits, and an extra 7 weeks was recently added.');
+    });
+
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'certify', ['message'], async (bot, message) => {
+        await bot.reply(message, 'After your account is set up, you must certify for your benefit payments every two weeks. This tells us if you are still unemployed and eligible to receive payments.');
+    });
+
+    controller.hears(new RegExp(/\b(?:certify|certify for benefits|get benefits|certification)\b/gi), 'message', async (bot, message) => {
+
+        await bot.reply(message, {
+            text: 'After your account is set up, you must certify for your benefit payments every two weeks. This tells us if you are still unemployed and eligible to receive payments.'
+        });
+    });
+
+    controller.hears(new RegExp(/\b(?:payment|where's my payment|where payment|pay|money|missing benefits|didn't get benefits|no benefits|did not get benefits)\b/gi), 'message', async (bot, message) => {
+
+        await bot.reply(message, {
+            text: 'It will take about 10 days to process your certification if you submitted it by mail and/or requested your benefits by check. If it has been more than 10 days ...'
+        });
+    });
+
+    controller.hears(new RegExp(/\b(?:site not working|tech issues|tech problems|technical issue|help with site|technical support)\b/gi), 'message', async (bot, message) => {
+
+        await bot.reply(message, {
+            text: 'To get technical help with registration, password resets, EDD Account Numbers, and how to use UI Online : Call 1-833-978-2511 (English and Spanish) from 8 AM to 8 PM pst.'
+        });
+    });
 
 }
+
--- features/websocket_features.js
+++ features/websocket_features.js
@@ -2,24 +2,32 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-module.exports = function(controller) {
+module.exports = function (controller) {
 
     if (controller.adapter.name === 'Web Adapter') {
 
         console.log('Loading sample web features...');
 
-        controller.hears(new RegExp('quick'), 'message', async (bot, message) => {
+        controller.hears(new RegExp(/\b(?:hello|hi|howdy|greetings|hola|oi|hey|quick|options|help|start)\b/gi), 'message', async (bot, message) => {
 
-            await bot.reply(message,{
-                text: 'Here are some options you can choose',
+            await bot.reply(message, {
+                text: 'Hi there, how can we help you? Here are some options you can choose',
                 quick_replies: [
                     {
-                        title: 'Payments',
-                        payload: 'payments',
+                        title: 'COVID-19',
+                        payload: 'covid-19',
                     },
                     {
-                        title: 'Help',
-                        payload: 'help',
+                        title: 'Tech Issues',
+                        payload: 'tech issues',
+                    },
+                    {
+                        title: 'Where\'s my Payment?',
+                        payload: 'payments'
+                    },
+                    {
+                        title: 'Certify for Benefits',
+                        payload: 'certify'
                     }
                 ]
             });
